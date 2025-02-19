269b18b27158d2a383d3a170817813b4b22873d2
heyscottn <heyscottn@gmail.com>
2025-02-19 13:21:21 -0600

    mm

--- try2.py
+++ try2.py
@@ -2,7 +2,7 @@
 print("\n--- last part, the constitution ---")
 # --- load the data ---
 s = ''
-f = open('u13dict2/constitution.txt', "r")
+f = open('constitution.txt', "r")
 s = f.read()
 f.close()
 
@@ -10,15 +10,27 @@ f.close()
 print("\nlength of string:")
 print("a sample from the string:")
 
+
 # clean up the data
 unwanted = ["\n", ".", "!", ",", "?", ":"]
 
 print("\na sample after cleaning:")
 
+
+
 # convert to a list of words
+words = []
+print("\na sample of the list:")
+print("length of words:")
+
 
 
 # create a list of unique words
+unique_words = []
+
+print("\na sample of unique:", unique_words[:10])
+print("length of unique words:", len(unique_words))
+
 
 
 # make a list of words that occur only once
@@ -32,9 +44,17 @@ print("\nhere are the words that occur only once:", only_once)
 
 # create a dictionary
 # key=word: value=count of that word
+counts = {}
 
+
+print("here's the counts:", counts)
 
 
 
 # what is the most common word?
+max_count = 0
+max_word = ''
+
+print("\n", max_word, "is the most common")
+print(max_count, "times")
 
